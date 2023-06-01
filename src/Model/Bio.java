59575db60b28fd3e76e9b112425f4d7ec0ac6e6e
hasti <hastikabirsamadi@gmail.com>
2023-06-01 11:54:17 +0330

    define fields, constructor, getters and setters for Bio class

--- src/Model/Bio.java
+++ src/Model/Bio.java
@@ -1,5 +1,37 @@
 package Model;
 
 public class Bio {
+    private StringBuilder website;
+    private String location;
+    private String details;
 
+    public Bio(StringBuilder website, String location, String details) {
+        this.website = website;
+        this.location = location;
+        this.details = details;
+    }
+
+    public StringBuilder getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(StringBuilder website) {
+        this.website = website;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 }
