d815248bce3a1dbe292aae2068e897713509bf52
kiana <kiana.sherafati@gmail.com>
2023-06-03 14:46:13 +0330

    Completed toString method in Tweet class

--- src/Model/Tweet.java
+++ src/Model/Tweet.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 public class Tweet implements Serializable {
     private StringBuilder body;
@@ -47,18 +48,18 @@ public class Tweet implements Serializable {
 
     @Override
     public String toString() {
-        long diff = ChronoUnit.SECONDS.between(this.tweetTime, LocalDate.now());
+        long diff = convertDate(tweetTime, LocalDate.now());
         if(0 <= diff && diff < 60) {
             this.currentTime = "Just Now";
         }
-        if(0 < diff &&  Duration.between(tweetTime , LocalDate.now()).toMinutes() < 60) {
-            this.currentTime = String.valueOf(this.tweetTime.until(LocalDate.now(), ChronoUnit.MINUTES));
+        else if(0 < diff/60 && diff/60 < 60) {
+            this.currentTime = (diff/60) + "m";
         }
-        else if(0 < this.tweetTime.until(LocalDate.now(), ChronoUnit.HOURS) && this.tweetTime.until(LocalDate.now(), ChronoUnit.HOURS) <= 24) {
-            this.currentTime = String.valueOf(this.tweetTime.until(LocalDate.now(), ChronoUnit.HOURS));
+        else if(0 < diff/3600 && diff/3600 <= 24) {
+            this.currentTime = (diff/3600) + "h";
         }
         else {
-            this.currentTime = String.valueOf(LocalDate.now());
+            this.currentTime = String.valueOf(LocalDate.now().getDayOfYear()) + (LocalDate.now().getMonth());
         }
         return "*********************************" + "\n" +
                 body +
@@ -66,5 +67,11 @@ public class Tweet implements Serializable {
                 "   retweets : " + retweets +
                 "   comments=" + comments +
                 "\n" + currentTime;
+    }
+
+    public static long convertDate(LocalDate startTime, LocalDate finishTime){
+        Date start = java.sql.Date.valueOf(startTime);
+        Date finish = java.sql.Date.valueOf(finishTime);
+        return (finish.getTime() - start.getTime())/1000;
     }
 }
